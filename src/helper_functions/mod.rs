4de2190e431e41bc76b8a1fb66e12da964a959b9
VPRamon <vallespuigramon@gmail.com>
2025-08-24 15:51:53 +0200

    mv reports

--- src/helper_functions/mod.rs
+++ src/helper_functions/mod.rs
@@ -12,9 +12,11 @@ pub mod queries;
 pub mod mutations;
 pub mod filters;
 pub mod decorators;
+pub mod reports;
 
 pub use auth::*;
 pub use queries::*;
 pub use mutations::*;
 pub use filters::*;
+pub use reports::*;
 pub use decorators::*;

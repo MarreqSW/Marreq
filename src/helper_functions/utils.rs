3f85cbd9b36bbac20f49f5af764edd158574e356
VPRamon <vallespuigramon@gmail.com>
2025-09-04 23:41:12 +0200

    test utils

--- src/helper_functions/utils.rs
+++ src/helper_functions/utils.rs
@@ -43,3 +43,49 @@ pub fn generate_requirement_reference(category_id: i32, project_id: i32) -> Resu
 
     Ok(reference)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rocket::http::Cookie;
+    use rocket::local::blocking::Client;
+    use rocket::{get, routes};
+
+    #[get("/")]
+    fn read_cookie_route(cookies: &CookieJar<'_>) -> String {
+        get_selected_project_id(cookies)
+            .map(|id| id.to_string())
+            .unwrap_or_else(|| "none".into())
+    }
+
+    #[test]
+    fn get_selected_project_id_returns_id() {
+        let rocket = rocket::build().mount("/", routes![read_cookie_route]);
+        let client = Client::untracked(rocket).expect("valid rocket instance");
+        let response = client
+            .get("/")
+            .cookie(Cookie::new("selected_project_id", "42"))
+            .dispatch();
+        assert_eq!(response.into_string().unwrap(), "42");
+    }
+
+    #[test]
+    fn get_selected_project_id_missing_cookie() {
+        let rocket = rocket::build().mount("/", routes![read_cookie_route]);
+        let client = Client::untracked(rocket).expect("valid rocket instance");
+        let response = client.get("/").dispatch();
+        assert_eq!(response.into_string().unwrap(), "none");
+    }
+
+    #[test]
+    fn get_selected_project_id_invalid_cookie() {
+        let rocket = rocket::build().mount("/", routes![read_cookie_route]);
+        let client = Client::untracked(rocket).expect("valid rocket instance");
+        let response = client
+            .get("/")
+            .cookie(Cookie::new("selected_project_id", "abc"))
+            .dispatch();
+        assert_eq!(response.into_string().unwrap(), "none");
+    }
+
+}

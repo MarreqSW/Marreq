c7f93c015a747db75e15f1c71906073bd8987690
ramon.valles <ramon.valles-puig@cta-consortium.org>
2025-09-04 17:38:23 +0200

    remove target in excel_parser

--- excel_parser/target/debug/deps/futures_channel-3584ae96d83994d5.d
+++
@@ -1,9 +0,0 @@
-/home/marius/Work/ReqMan/excel_parser/target/debug/deps/futures_channel-3584ae96d83994d5.d: /home/marius/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-channel-0.3.31/src/lib.rs /home/marius/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-channel-0.3.31/src/lock.rs /home/marius/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-channel-0.3.31/src/mpsc/mod.rs /home/marius/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-channel-0.3.31/src/mpsc/queue.rs /home/marius/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-channel-0.3.31/src/oneshot.rs
-
-/home/marius/Work/ReqMan/excel_parser/target/debug/deps/libfutures_channel-3584ae96d83994d5.rmeta: /home/marius/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-channel-0.3.31/src/lib.rs /home/marius/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-channel-0.3.31/src/lock.rs /home/marius/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-channel-0.3.31/src/mpsc/mod.rs /home/marius/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-channel-0.3.31/src/mpsc/queue.rs /home/marius/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-channel-0.3.31/src/oneshot.rs
-
-/home/marius/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-channel-0.3.31/src/lib.rs:
-/home/marius/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-channel-0.3.31/src/lock.rs:
-/home/marius/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-channel-0.3.31/src/mpsc/mod.rs:
-/home/marius/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-channel-0.3.31/src/mpsc/queue.rs:
-/home/marius/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-channel-0.3.31/src/oneshot.rs:
--- excel_parser/target/release/deps/foreign_types-87a1ded147699d51.d
+++
@@ -1,7 +0,0 @@
-/home/marius/Work/ReqMan/excel_parser/target/release/deps/foreign_types-87a1ded147699d51.d: /home/marius/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/foreign-types-0.3.2/src/lib.rs
-
-/home/marius/Work/ReqMan/excel_parser/target/release/deps/libforeign_types-87a1ded147699d51.rlib: /home/marius/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/foreign-types-0.3.2/src/lib.rs
-
-/home/marius/Work/ReqMan/excel_parser/target/release/deps/libforeign_types-87a1ded147699d51.rmeta: /home/marius/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/foreign-types-0.3.2/src/lib.rs
-
-/home/marius/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/foreign-types-0.3.2/src/lib.rs:

b7d2339256af116c3e7f4e08ccf60facf41d6d3c
vpramon <vallespuigramon@gmail.com>
2025-09-27 17:16:41 +0200

    Implement State<AppState> to include DB and avoid DieselCachedRepo globals

--- src/app.rs
+++ src/app.rs
@@ -1,21 +1,26 @@
-use rocket::fs::{relative, FileServer};
+use std::sync::{Arc, RwLock};
 use rocket::{Build, Rocket};
-use rocket_dyn_templates::Template;
+use crate::repository::{DieselRepo, DieselCachedRepo};
 
-use crate::api;
-use crate::html::cors::CorsFairing;
-use crate::routes::{catchers, html};
+#[derive(Clone)]
+pub struct AppState {
+    pub repo: Arc<RwLock<DieselCachedRepo>>,
+}
 
 #[rocket_sync_db_pools::database("my_db")]
 pub struct MyDbConn(rocket_sync_db_pools::diesel::PgConnection);
 
 pub fn build() -> Rocket<Build> {
+    let cached = DieselCachedRepo::new(DieselRepo::new(), 5 * 60);
+    let repo = Arc::new(RwLock::new(cached));
+
     rocket::build()
-        .mount("/", html::routes())
-        .mount("/api/v1", api::routes())
-        .register("/", catchers![catchers::unauthorized, catchers::forbidden])
-        .mount("/static", FileServer::from(relative!("src/html/static")))
-        .attach(CorsFairing)
-        .attach(Template::fairing())
-        .attach(MyDbConn::fairing())
+        .manage(AppState { repo })
+        .mount("/", crate::routes::html::routes())
+        .mount("/api", crate::api::routes())
+        .register("/", catchers![crate::routes::catchers::unauthorized, crate::routes::catchers::forbidden])
+        .mount("/static", rocket::fs::FileServer::from(rocket::fs::relative!("src/html/static")))
+        .attach(crate::html::cors::CorsFairing)
+        .attach(rocket_dyn_templates::Template::fairing())
+        .attach(crate::app::MyDbConn::fairing())
 }

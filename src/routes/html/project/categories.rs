d4731f069dce720fb8de9680457bd1e79702a01e
vpramon <vallespuigramon@gmail.com>
2025-10-05 20:31:35 +0200

    use category service

--- src/routes/html/project/categories.rs
+++ src/routes/html/project/categories.rs
@@ -1,5 +1,6 @@
 use super::helpers::*;
 use super::prelude::*;
+use crate::services::CategoryService;
 
 #[get("/<project_id>/categories")]
 async fn show_categories(
@@ -9,10 +10,8 @@ async fn show_categories(
 ) -> Result<Template, Redirect> {
     let user = project_access.into_user();
     let projects = get_accessible_projects(state, &user);
-    let categories = state
-        .repo_read()
-        .get_categories_by_project(project_id)
-        .unwrap_or_default();
+    let service = CategoryService::new(state.inner());
+    let categories = service.list_by_project(project_id).unwrap_or_default();
 
     let ctx = json!({
         "user": user,
@@ -48,7 +47,8 @@ async fn post_category(
     new_category: Form<NewCategory>,
     state: &State<AppState>,
 ) -> Result<Redirect, Redirect> {
-    let user_id = project_access.into_user().user_id;
+    let user = project_access.into_user();
+    let service = CategoryService::new(state.inner());
 
     let new_url = uri!("/p", new_category(project_id));
     let show_url = uri!("/p", show_categories(project_id));
@@ -56,20 +56,10 @@ async fn post_category(
     let mut category = new_category.into_inner();
     category.project_id = project_id;
 
-    let category_id = state
-        .repo_write()
-        .insert_new_category(&category)
-        .map_err(|_e| {
-            #[cfg(debug_assertions)]
-            eprintln!("insert_new_category error: {:?}", _e);
-            Redirect::to(new_url.clone())
-        })?;
-
-    if let Ok(mut conn) = get_db_connection(state) {
-        if let Ok(full) = state.repo_read().get_category_by_id(category_id) {
-            let log_ctx = LogCtx::new(user_id);
-            let _ = Logger::created(&mut conn, &log_ctx, category_id, &full);
-        }
+    if let Err(_e) = service.create(&user, category) {
+        #[cfg(debug_assertions)]
+        eprintln!("insert_new_category error: {:?}", _e);
+        return Ok(Redirect::to(new_url.clone()));
     }
 
     Ok(Redirect::to(show_url))
@@ -83,10 +73,10 @@ async fn get_edit_category(
     state: &State<AppState>,
 ) -> Result<Template, Redirect> {
     let user = project_access.into_user();
+    let service = CategoryService::new(state.inner());
 
-    let category = state
-        .repo_read()
-        .get_category_by_id(cat_id)
+    let category = service
+        .get_by_id(cat_id)
         .map_err(|_| Redirect::to(uri!("/p", show_categories(project_id))))?;
 
     if category.project_id != project_id {
@@ -116,14 +106,14 @@ async fn post_edit_category(
     category: Form<NewCategory>,
     state: &State<AppState>,
 ) -> Result<Redirect, Redirect> {
-    let user_id = project_access.into_user().user_id;
+    let user = project_access.into_user();
+    let service = CategoryService::new(state.inner());
 
     let edit_url = uri!("/p", get_edit_category(project_id, cat_id));
     let show_url = uri!("/p", show_categories(project_id));
 
-    let old = state
-        .repo_read()
-        .get_category_by_id(cat_id)
+    let old = service
+        .get_by_id(cat_id)
         .map_err(|_| Redirect::to(show_url.clone()))?;
 
     if old.project_id != project_id {
@@ -137,17 +127,10 @@ async fn post_edit_category(
     edited.cat_id = Some(cat_id);
     edited.project_id = project_id;
 
-    state.repo_write().edit_category(&edited).map_err(|_e| {
+    if let Err(_e) = service.update(&user, cat_id, edited) {
         #[cfg(debug_assertions)]
         eprintln!("edit_category error: {:?}", _e);
-        Redirect::to(edit_url.clone())
-    })?;
-
-    if let Ok(mut conn) = get_db_connection(state) {
-        if let Ok(new_row) = state.repo_read().get_category_by_id(cat_id) {
-            let log_ctx = LogCtx::new(user_id);
-            let _ = Logger::updated(&mut conn, &log_ctx, &old, &new_row);
-        }
+        return Ok(Redirect::to(edit_url.clone()));
     }
 
     Ok(Redirect::to(show_url))
@@ -160,9 +143,10 @@ async fn delete_category_route(
     cat_id: i32,
     state: &State<AppState>,
 ) -> Result<rocket::http::Status, Redirect> {
-    let user_id = project_access.into_user().user_id;
+    let user = project_access.into_user();
+    let service = CategoryService::new(state.inner());
 
-    let category = match state.repo_read().get_category_by_id(cat_id) {
+    let category = match service.get_by_id(cat_id) {
         Ok(c) => c,
         Err(_) => return Ok(rocket::http::Status::NotFound),
     };
@@ -174,21 +158,14 @@ async fn delete_category_route(
         )));
     }
 
-    let deleted = match state.repo_write().delete_category(cat_id) {
-        Ok(c) => c,
+    match service.delete(&user, cat_id) {
+        Ok(_) => Ok(rocket::http::Status::Ok),
         Err(_e) => {
             #[cfg(debug_assertions)]
             eprintln!("delete_category error: {:?}", _e);
-            return Ok(rocket::http::Status::InternalServerError);
+            Ok(rocket::http::Status::InternalServerError)
         }
-    };
-
-    if let Ok(mut conn) = get_db_connection(state) {
-        let log_ctx = LogCtx::new(user_id);
-        let _ = Logger::deleted(conn.as_mut(), &log_ctx, &deleted);
     }
-
-    Ok(rocket::http::Status::Ok)
 }
 
 pub fn routes() -> Vec<Route> {
